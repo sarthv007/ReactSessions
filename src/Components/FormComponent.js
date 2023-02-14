e9e67252260143b978053cee42dd733821fb3dc2
sarthak vaidya <sarthakvaidya0@gmail.com>
2023-02-14 08:58:29 +0530

    validation fixed

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -8,7 +8,7 @@ export class FormComponent extends Component {
     this.state = {
       firstName: "",
       lastName: "",
-      country: "usa",
+      country: "",
       error: {
         firstNameError: "",
         lasstNameError: "",
@@ -48,35 +48,30 @@ export class FormComponent extends Component {
   };
 
   validate = () => {
+    this.setState({
+      error: {
+        firstNameError: "",
+        lasstNameError: "",
+        countryError: "",
+      },
+    });
     let flag = true;
-    let err = {
-      firstName: "",
-      lastName: "",
-      country: "",
-    };
+    let err = {};
     const { firstName, lastName, country, error } = this.state;
     if (firstName === "") {
       flag = false;
-      err = {
-        ...err,
-        firstName: "Please enter firstName",
-      };
+      err["firstNameError"] = "Please enter firstName";
     }
     if (lastName === "") {
       flag = false;
-      err = {
-        ...err,
-        lastName: "Please enter lastName",
-      };
+      err["lastNameError"] = "Please enter lastName";
     }
     if (country === "") {
       flag = false;
-      err = {
-        ...err,
-        country: "Please enter country",
-      };
+      err["countryError"] = "Please enter country";
     }
     if (flag === false) {
+      console.log(err);
       this.setState({
         error: err,
       });
@@ -85,6 +80,7 @@ export class FormComponent extends Component {
 
   render() {
     const { firstName, lastName, country, error } = this.state;
+    console.log(error);
     return (
       <>
         <h3>Using CSS to style an HTML Form</h3>
@@ -99,7 +95,9 @@ export class FormComponent extends Component {
             onChange={this.handleFirstNameChange}
             placeholder="Your name.."
           />
-          <p className="error">{error.firstName && error.firstName}</p>
+          <p className="error">
+            {error.firstNameError && error.firstNameError}
+          </p>
 
           <label htmlFor="lname">Last Name</label>
           <input
@@ -110,7 +108,7 @@ export class FormComponent extends Component {
             onChange={this.handleLastNameChange}
             placeholder="Your last name.."
           />
-          <p className="error">{error.lastName && error.lastName}</p>
+          <p className="error">{error.lastNameError && error.lastNameError}</p>
 
           <label htmlFor="country">Country</label>
           <select
@@ -119,11 +117,12 @@ export class FormComponent extends Component {
             onChange={this.handleCountryChange}
             value={country}
           >
+            <option value="">Please Select</option>
             <option value="australia">Australia</option>
             <option value="canada">Canada</option>
             <option value="usa">USA</option>
           </select>
-          <p className="error">{error.country && error.country}</p>
+          <p className="error">{error.countryError && error.countryError}</p>
 
           <input type="submit" value="Submit" />
         </form>
